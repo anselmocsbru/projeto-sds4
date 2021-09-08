89f1f8f9c8567e8aced9579983698a8c1d92edc6
Anselmo Santos <anselmocs@hotmail.com>
2021-09-08 01:39:04 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary"> Olá a todos da Semana Spring React </h1>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary"> Olá pessoal da Semana Spring React 4.0</h1>
+
+        <DataTable />
     </div>
-  );
+    <Footer />
+    </>
+        );
 }
 
 export default App;
